7aefda9cb63b108040dfca6b11c8dc64fb624b05
ljia310 <ljia310@gmail.com>
2017-05-25 23:46:24 +0800

    资源服务增加忽略路径

--- srv1-server/src/main/java/com/cloud/Srv1ServerApplication.java
+++ srv1-server/src/main/java/com/cloud/Srv1ServerApplication.java
@@ -1,20 +1,39 @@
 package com.cloud;
 
+import io.swagger.models.HttpMethod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
-@EnableResourceServer
+
 public class Srv1ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Srv1ServerApplication.class, args);
 	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.anonymous().and()
+					.authorizeRequests()
+					.antMatchers("/hystrix/**","info").permitAll()
+					.anyRequest().authenticated();
+		}
+
+	}
 }
+
+
